0a981523bd43c5f893faec1c0103e8caa9039e14
Anas <131682794+Anas1010101@users.noreply.github.com>
2026-01-19 03:46:30 +0100

    Ajout du provider AWS et du VPC

--- Project/foundation/main.tf
+++ Project/foundation/main.tf
@@ -1,79 +1,21 @@
+############################################
+# 1. Provider AWS
+############################################
+# Indique à Terraform quel fournisseur cloud utiliser
+# et dans quelle région déployer les ressources.
 provider "aws" {
-  region = "eu-west-3"
+  region = "eu-west-3" # Région Paris
 }
 
-
+############################################
+# 2. Virtual Private Cloud (VPC)
+############################################
+# Création d’un réseau privé isolé dans AWS
+# 10.0.0.0/16 permet jusqu’à 65 536 adresses IP
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  tags = { Name = "vpc-examen" }
-}
 
-
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  tags = { Name = "subnet-examen" }
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-  tags = { Name = "igw-examen" }
-}
-
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-}
-
-# 5. Route Table Association (Ressource 8)
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.rt.id
-}
-
-# 6. Security Group
-resource "aws_security_group" "allow_web" {
-  name        = "allow_web_traffic"
-  vpc_id      = aws_vpc.main.id
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-
-resource "aws_ecr_repository" "app" {
-  name = "repo-examen"
-}
-
-
-resource "aws_iam_role" "eks_role" {
-  name = "eks-cluster-role-examen"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = { Service = "eks.amazonaws.com" }
-    }]
-  })
-}
-
-resource "aws_eks_cluster" "main" {
-  name     = "cluster-examen"
-  role_arn = aws_iam_role.eks_role.arn
-  vpc_config {
-    subnet_ids = [aws_subnet.public.id]
+  tags = {
+    Name = "vpc-examen"
   }
 }
