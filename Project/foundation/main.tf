fe69aceb64c981b6f816c93a1df9ee7670c10f13
Anas <131682794+Anas1010101@users.noreply.github.com>
2026-01-19 03:47:28 +0100

    Ajout de l'accés à Internet / Table de Routage

--- Project/foundation/main.tf
+++ Project/foundation/main.tf
@@ -19,3 +19,31 @@ resource "aws_vpc" "main" {
     Name = "vpc-examen"
   }
 }
+
+############################################
+# 3. Internet Gateway
+############################################
+# Permet aux ressources du VPC d’accéder à Internet
+# (obligatoire pour un cluster EKS public)
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "igw-examen"
+  }
+}
+
+############################################
+# 4. Table de routage
+############################################
+# Définit les règles de routage du VPC
+# Ici, tout le trafic sortant (0.0.0.0/0)
+# passe par l’Internet Gateway
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
